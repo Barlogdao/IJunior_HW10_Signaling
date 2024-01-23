dee5e76096fc5d8ea80556e81ce20266c56b3ac1
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2024-01-23 18:14:23 +0300

    Do Alarm Sound

--- Assets/Scripts/Signalization.cs
+++ Assets/Scripts/Signalization.cs
@@ -1,20 +1,39 @@
 using UnityEngine;
 
+[RequireComponent (typeof(AudioSource))]
 public class Signalization : MonoBehaviour
 {
     [SerializeField] private Light _alarmLight;
-    [SerializeField] private float _maxLightIntensity = 7;
+    [SerializeField] private float _lightIntesityRate;
+    [SerializeField] private float _soundVolumeRate;
+
+    private AudioSource _audiosource;
+    private float _minSoundVolume = 0f;
+    private float _maxSoundVolume = 1f;
+
+    private float _minLightIntensity = 0f;
+    private float _maxLightIntensity = 7;
+
+    private bool _isThiefInHouse = false;
 
     private void Awake()
     {
-        _alarmLight.intensity = 0f;
+        _audiosource = GetComponent<AudioSource>();
+        _audiosource.volume = _minSoundVolume;
+        _alarmLight.intensity = _minLightIntensity;
+    }
+
+    private void Update()
+    {
+        UpdateAlarmLight();
+        UpdateAlarmSound();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.TryGetComponent(out ThiefMover thief))
+        if (other.TryGetComponent(out ThiefMover thief))
         {
-            _alarmLight.intensity = _maxLightIntensity;
+            _isThiefInHouse = true;
         }
     }
 
@@ -22,7 +41,20 @@ public class Signalization : MonoBehaviour
     {
         if (other.TryGetComponent(out ThiefMover thief))
         {
-            _alarmLight.intensity = 0f;
+            _isThiefInHouse = false;
         }
+    }
+
+    private void UpdateAlarmLight()
+    {
+        float targetLightIntensity = _isThiefInHouse == true ? _maxLightIntensity : _minLightIntensity;
+
+        _alarmLight.intensity = Mathf.MoveTowards(_alarmLight.intensity, targetLightIntensity, _lightIntesityRate * Time.deltaTime);
+    }
+    private void UpdateAlarmSound()
+    {
+        float targetSoundVolume = _isThiefInHouse == true ? _maxSoundVolume: _minSoundVolume;
+
+        _audiosource.volume = Mathf.MoveTowards(_audiosource.volume, targetSoundVolume, _soundVolumeRate * Time.deltaTime);
     }
 }
